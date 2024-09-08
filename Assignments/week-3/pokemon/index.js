df3157bd77800006ecef1da4795d62680adb85a1
shivanshu0055 <shivanshu192004@gmail.com>
2024-09-09 01:21:50 +0530

    optimise pkemon assignment

--- Assignments/week-3/pokemon/index.js
+++ Assignments/week-3/pokemon/index.js
@@ -26,35 +26,38 @@ function findPokes(){
  
 const numOfPoke=parseInt(document.querySelector("#numOfPoke").value)
 const typeOfPoke=document.querySelector("#typeOfPoke").value
+
 async function fetchProcess(){
     let n=0;
     let pokenum=1
     let url
     pokes=[]
-    while(n<numOfPoke && pokenum<=200){
-    url=`https://pokeapi.co/api/v2/pokemon/${pokenum}`
-        let cnt=0
-        const response=await fetch(url)
-        const data=await response.json()
-        data.types.forEach((X)=>{
+    const promises = [];
+
+    while (n < numOfPoke && pokenum <= 500) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokenum}`;
+        promises.push(fetch(url).then(response => response.json()));
+        pokenum++;
+    }
+
+       const arr=await Promise.all(promises)
+       arr.forEach((pokedata)=>{
+        if(n<numOfPoke){
+        pokedata.types.forEach((X)=>{
             if(X.type.name==typeOfPoke){
-                pokes.push(data);
+                pokes.push(pokedata)
                 n++;
             }
         })
-        pokenum++
-    
     }
-    console.log(n);
-    console.log(pokenum);
-    document.querySelector("#pokedex").innerHTML=""
-    render()
+       })
+        // await sleep(200);
+        console.log(n);
+        console.log(pokenum);
+        document.querySelector("#pokedex").innerHTML=""
+        render()
+    }
+    fetchProcess();
+    console.log(pokes);
 
 }
-fetchProcess();
-console.log(pokes);
-}
-
-
-
- 
